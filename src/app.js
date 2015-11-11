7ea8b6fdaf1af98a0cbd24852e7ddd3a898c925c
Chris Wong <chris@cogifire.com>
2015-11-11 11:04:36 -0800

    ui

--- src/app.js
+++ src/app.js
@@ -6,55 +6,47 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
-});
+var locationOptions = {
+  maximumAge: 10000, 
+  timeout: 10000
+};
 
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
+function locationSuccess(pos) {
+  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+  var locations = [
+    {
+      title: 'Peets',
+      subtitle: '250 feet away'
+    }
+  ];
+  
+  var main = new UI.Menu({
     sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
+      title: 'Nearby coffee shops',
+      items: locations
     }]
   });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+  
+  main.show();
+  
+  main.on('select', function(e) {
+    var details = new UI.Card();
+    details.title(locations[e.itemIndex].title);
+    details.subtitle(locations[e.itemIndex].subtitle);
+    details.body('test');
+    details.show();
   });
-  menu.show();
-});
+}
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+function locationError(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+Pebble.addEventListener('ready',
+  function(e) {
+    // Request current position
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  }
+);
