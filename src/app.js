c363452bc2a2daf9b8e01320867b07a350b6dfb7
Chris Wong <chris@cogifire.com>
2015-11-11 13:11:50 -0800

    Google Places API

--- src/app.js
+++ src/app.js
@@ -13,31 +13,17 @@ var locationOptions = {
   timeout: 10000
 };
 
+var id;
+
+var locationOptions = {
+  enableHighAccuracy: true, 
+  maximumAge: 0, 
+  timeout: 5000
+};
+
 function locationSuccess(pos) {
+  console.log('Location changed!');
   console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  var locations = [
-    {
-      title: 'Peets',
-      subtitle: '250 feet away'
-    }
-  ];
-  
-  var main = new UI.Menu({
-    sections: [{
-      title: 'Nearby coffee shops',
-      items: locations
-    }]
-  });
-  
-  main.show();
-  
-  main.on('select', function(e) {
-    var details = new UI.Card();
-    details.title(locations[e.itemIndex].title);
-    details.subtitle(locations[e.itemIndex].subtitle);
-    details.body('test');
-    details.show();
-  });
 }
 
 function locationError(err) {
@@ -46,7 +32,51 @@ function locationError(err) {
 
 Pebble.addEventListener('ready',
   function(e) {
-    // Request current position
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+    // Get location updates
+    id = navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
   }
 );
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDM2Tl_TQVRohTGqoezx7v5Z1T_Djodetk&location=37.3169361%2C-122.0439579&radius=1000&keyword=coffee",
+  "method": "GET",
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "975f33c0-e625-1022-79ed-3646979faf40"
+  }
+};
+
+ajax(settings, function (response) {
+  var res = JSON.parse(response);
+  var locations = [res.results.length];
+  
+  console.log('location array declared');
+  
+  for (var i = 0; i < res.results.length; i++) {
+    locations[i] = { title: res.results[i].name, subtitle: res.results[i].vicinity };
+    //console.log(res.results[i].name);
+    //console.log(res.results[i].vicinity);
+    console.log(locations[i].title);
+  }
+  
+  console.log('loading ui');
+  var main = new UI.Menu({
+    sections: [{
+      title: 'Nearby coffee shops',
+      items: locations
+    }]
+  });
+
+  console.log('showing ui');
+  main.show();
+
+  main.on('select', function(e) {
+    var details = new UI.Card();
+    details.title(locations[e.itemIndex].title);
+    details.subtitle(locations[e.itemIndex].subtitle);
+    details.body('test');
+    details.show();
+  });
+});
