7106d7f6fd6efd4e7379464626fc5e5e6fb8ce4c
Chris Wong <chris@cogifire.com>
2015-11-11 18:57:35 -0800

    Display distance instead of address

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ function getCoffeeShops(latitude, longitude) {
     var locations = [res.results.length];
 
     for (var i = 0; i < res.results.length; i++) {
-      locations[i] = { title: res.results[i].name, subtitle: res.results[i].vicinity };
+      locations[i] = { title: res.results[i].name, subtitle: getDistance(latitude, res.results[i].geometry.location.lat, longitude, res.results[i].geometry.location.lng) };
     }
     
     displayUI(locations);
@@ -53,4 +53,21 @@ function displayUI(locations) {
     details.body('test');
     details.show();
   });
+}
+
+function getDistance(lat1, lat2, lon1, lon2) {
+  var radlat1 = Math.PI * lat1/180;
+	var radlat2 = Math.PI * lat2/180;
+	var theta = lon1-lon2;
+	var radtheta = Math.PI * theta/180;
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist);
+	dist = dist * 180/Math.PI;
+	dist = dist * 60 * 1.1515;
+  dist = dist.toFixed(2);
+  
+  if (dist >= (1000/5280))
+    return dist + ' miles away';
+  else
+    return (dist * 5280) + ' feet away';
 }
